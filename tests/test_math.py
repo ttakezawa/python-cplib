381f63ea1973abd996dd374d66af24304edb7a70
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-13 02:44:56 +0900

    Fix format

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
+
 from cplib.math import gcd
 
+
 class Test(TestCase):
-    def test_gcd(self):
-        assert gcd(12,8) == 4
-        assert gcd(12,6) == 6
-        assert gcd(0,42) == 42
+    def test_gcd(self) -> None:
+        assert gcd(12, 8) == 4
+        assert gcd(12, 6) == 6
+        assert gcd(0, 42) == 42
