4341a1a7b08438145b2f233e6354e5f4c51361fa
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-13 03:17:48 +0900

    Add math.lcm

--- cplib/math.py
+++ cplib/math.py
@@ -2,3 +2,7 @@ def gcd(a: int, b: int) -> int:
     while b:
         a, b = b, a % b
     return a
+
+
+def lcm(a: int, b: int) -> int:
+    return a // gcd(a, b) * b
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from cplib.math import gcd
+from cplib.math import gcd, lcm
 
 
 class Test(TestCase):
@@ -8,3 +8,8 @@ class Test(TestCase):
         assert gcd(12, 8) == 4
         assert gcd(12, 6) == 6
         assert gcd(0, 42) == 42
+
+    def test_lcm(self) -> None:
+        assert lcm(8, 10) == 40
+        assert lcm(3, 5) == 15
+        assert lcm(0, 42) == 0
