e197e698be90080532b59029e1614c1ce9378fe4
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-21 03:51:36 +0900

    Add modulo

---
+++ cplib/modulo.py
@@ -0,0 +1,73 @@
+from typing import Tuple
+
+mod1000000007 = 1000000007
+mod998244353 = 998244353
+
+
+def pow_mod(x: int, n: int, mod: int) -> int:
+    if mod == 1:
+        return 0
+    x %= mod
+    r = 1
+    while n:
+        if n & 1:
+            r = r * x % mod
+        x = x * x % mod
+        n >>= 1
+    return r
+
+
+def inv_mod(x: int, mod: int) -> int:
+    """
+    引数xとmodは互いに素 GCD(a,mod)=1 であることが必要
+    """
+    assert 1 <= mod
+    z = _inv_gcd(x, mod)
+    assert z[0] == 1
+    return z[1]
+
+
+def _inv_gcd(a: int, b: int) -> Tuple[int, int]:
+    a %= b
+    if a == 0:
+        return (b, 0)
+    s, t = b, a
+    m0, m1 = 0, 1
+    while t:
+        u = s // t
+        s -= t * u
+        m0 -= m1 * u
+        s, t = t, s
+        m0, m1 = m1, m0
+    if m0 < 0:
+        m0 += b // s
+    return (s, m0)
+
+
+class FactorialCache:
+    __slots__ = (
+        "mod",
+        "_val",
+        "_inv",
+    )
+
+    def __init__(self, max: int, mod: int) -> None:
+        self.mod = mod
+        self._val = [1] * (max + 1)
+        self._inv = [1] * (max + 1)
+        for i in range(2, max + 1):
+            self._val[i] = self._val[i - 1] * i % mod
+        self._inv[max] = inv_mod(self._val[max], mod)
+        for i in reversed(range(3, max + 1)):
+            self._inv[i - 1] = self._inv[i] * i % mod
+
+    def combination(self, n: int, k: int) -> int:
+        if n < k:
+            return 0
+        return self._val[n] * self._inv[k] % self.mod * self._inv[n - k] % self.mod
+
+    def factorial(self, n: int) -> int:
+        return self._val[n]
+
+    def inv_factorial(self, n: int) -> int:
+        return self._inv[n]
---
+++ tests/test_modulo.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from cplib.modulo import FactorialCache, inv_mod, mod1000000007, pow_mod
+
+
+class Test(TestCase):
+    def test_pow_mod(self) -> None:
+        def naive(x: int, n: int, mod: int) -> int:
+            x %= mod
+            r = 1 % mod
+            for _ in range(n):
+                r = r * x % mod
+            return r
+
+        for a in range(-100, 70):
+            for b in range(0, 20):
+                for c in range(1, 101):
+                    assert naive(a, b, c) == pow_mod(a, b, c)
+
+    def test_inv_mod(self) -> None:
+        assert inv_mod(1, 13) == 1
+        assert inv_mod(2, 13) == 7
+        assert inv_mod(3, 13) == 9
+        assert inv_mod(4, 13) == 10
+        assert inv_mod(5, 13) == 8
+
+    def test_combination(self) -> None:
+        fc = FactorialCache(int(5e6), mod1000000007)
+        assert fc.combination(2, 3) == 0
+        assert fc.combination(2, 0) == 1
+        assert fc.combination(5, 2) == 10
+        assert fc.combination(7, 3) == 35
+        assert fc.factorial(100) == 437918130
