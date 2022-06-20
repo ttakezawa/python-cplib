103ac50a62c451c1e726ccc215ac2450d4054d27
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-21 00:56:41 +0900

    Add DSU

---
+++ cplib/dsu.py
@@ -0,0 +1,61 @@
+# https://github.com/not522/ac-library-python/blob/master/atcoder/dsu.py
+from typing import List
+
+
+class DSU:
+    def __init__(self, n: int = 0) -> None:
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a: int, b: int) -> int:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        x = self.leader(a)
+        y = self.leader(b)
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y = y, x
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+
+        return x
+
+    def same(self, a: int, b: int) -> bool:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        parent = self.parent_or_size[a]
+        while parent >= 0:
+            if self.parent_or_size[parent] < 0:
+                return parent
+            self.parent_or_size[a], a, parent = (
+                self.parent_or_size[parent],
+                self.parent_or_size[parent],
+                self.parent_or_size[self.parent_or_size[parent]],
+            )
+
+        return a
+
+    def size(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self) -> List[List[int]]:
+        leader_buf = [self.leader(i) for i in range(self._n)]
+
+        result: List[List[int]] = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+
+        return list(filter(lambda r: r, result))
---
+++ tests/test_dsu.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from cplib.dsu import DSU
+
+
+class Test(TestCase):
+    def test_dsu(self) -> None:
+        dsu = DSU(6)
+        dsu.merge(0, 4)
+        dsu.merge(1, 2)
+        dsu.merge(2, 3)
+        assert dsu.same(0, 4) == True
+        assert dsu.same(1, 3) == True
+        assert dsu.same(5, 5) == True
+        assert dsu.same(0, 5) == False
+        assert dsu.groups() == [[0, 4], [1, 2, 3], [5]]
