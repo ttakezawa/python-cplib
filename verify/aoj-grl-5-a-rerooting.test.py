a1026776a2fada0507b5215e084823fa89a10db4
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-24 02:43:41 +0900

    Add verify of rerooting dp

---
+++ verify/aoj-grl-5-a-rerooting.test.py
@@ -0,0 +1,27 @@
+# verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_A
+
+from cplib.rerooting_dp import rerooting_dp
+
+
+def main():
+    n = int(input())
+    edges = dict()
+    adj = [[] for _ in range(n)]
+    for _ in range(n - 1):
+        s, t, w = map(int, input().split())
+        edges[(s, t)] = w
+        edges[(t, s)] = w
+        adj[s].append(t)
+        adj[t].append(s)
+
+    dp = rerooting_dp(
+        adj,
+        identity=0,
+        merge=lambda x, y: max(x, y),
+        add_edge=lambda s, src, dst: s + edges[(src, dst)],
+        add_children=lambda s, v: s,
+    )
+    print(max(dp))
+
+
+main()
