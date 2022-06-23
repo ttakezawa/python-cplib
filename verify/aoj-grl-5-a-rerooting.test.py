56c8e8ac675af4c202a81b2736eb9275653575a5
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-24 04:15:14 +0900

    Update verify of rerooting dp

--- verify/aoj-grl-5-a-rerooting.test.py
+++ verify/aoj-grl-5-a-rerooting.test.py
@@ -1,12 +1,13 @@
 # verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_A
 
+from typing import Dict, List, Tuple
 from cplib.rerooting_dp import rerooting_dp
 
 
 def main():
     n = int(input())
-    edges = dict()
-    adj = [[] for _ in range(n)]
+    edges: Dict[Tuple[int, int], int] = dict()
+    adj: List[List[int]] = [[] for _ in range(n)]
     for _ in range(n - 1):
         s, t, w = map(int, input().split())
         edges[(s, t)] = w
