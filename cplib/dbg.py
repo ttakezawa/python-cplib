d1fd275ee0f35f97d3bb0305866e0ed1bbda0d5d
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-27 02:28:31 +0900

    Update dbg

--- cplib/dbg.py
+++ cplib/dbg.py
@@ -1,9 +1,21 @@
-import inspect, pathlib, pprint, sys
+import sys
+
+if len(sys.argv) > 0 and sys.argv[0] == "./Main.py":
+
+    def dbg(*objects: object) -> None:
+        pass
 
 
-def dbg(*objects: object) -> None:
-    caller = inspect.stack()[1]
-    path = pathlib.Path(caller.filename)
-    at = f"{path.parent.name}/{path.name}:{caller.lineno}"
-    repr = pprint.pformat(objects)
-    print(f"{at} => \033[1;41m{repr}\033[0m", file=sys.stderr)
+else:
+
+    import inspect
+    import pathlib
+    import pprint
+    import sys
+
+    def dbg(*objects: object) -> None:
+        caller = inspect.stack()[1]
+        path = pathlib.Path(caller.filename)
+        at = f"{path.parent.name}/{path.name}:{caller.lineno}"
+        repr = pprint.pformat(objects)
+        print(f"{at} => \033[1;41m{repr}\033[0m", file=sys.stderr)
