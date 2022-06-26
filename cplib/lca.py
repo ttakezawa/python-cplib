d2b94643c002bb01901565b30d38392991731ca1
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-26 20:04:54 +0900

    Update LCA

--- cplib/lca.py
+++ cplib/lca.py
@@ -1,46 +1,44 @@
-from importlib.machinery import FrozenImporter
 from typing import List
-
-POW_MAX = 30
 
 
 class LCAGraph:
-    __slots__ = ("adj", "root", "parents", "depths")
+    __slots__ = (
+        "depths",
+        "ancestors",
+    )
 
     def __init__(self, adj: List[List[int]], root: int = 0) -> None:
-        self.adj = adj
-        self.root = root
         self.depths = [0] * len(adj)
-        # parents[k][v] = 2^k nth parent of v
-        self.parents = [[-1] * len(adj) for _ in range(POW_MAX + 1)]
 
-        # prepare depths[v] and parents[0][v]
+        # prepare depths[v] and parents[v] with DFS
         self.depths[root] = 0
         st = [root]
+        parents = [root] * len(adj)
         while st:
             v = st.pop()
             for u in adj[v]:
-                if u == self.parents[0][v]:
+                if u == parents[v]:
                     continue
-                self.parents[0][u] = v
+                parents[u] = v
                 self.depths[u] = self.depths[v] + 1
                 st.append(u)
+        bit_length = max(self.depths).bit_length()
 
-        # prepare parents[k][v]
-        for k in range(POW_MAX):
-            for v in range(len(adj)):
-                self.parents[k + 1][v] = self.parents[k][self.parents[k][v]]
+        # prepare parents[k][v] = 2^k nth parent of v with doubling
+        self.ancestors = [parents]
+        for _ in range(1, bit_length):
+            self.ancestors.append([self.ancestors[-1][v] for v in self.ancestors[-1]])
 
     def lca(self, v: int, u: int) -> int:
         if self.depths[v] < self.depths[u]:
             v, u = u, v
         df = self.depths[v] - self.depths[u]
-        for i in range(POW_MAX + 1):
+        for i in range(df.bit_length()):
             if df >> i & 1 == 1:
-                v = self.parents[i][v]
+                v = self.ancestors[i][v]
         if v == u:
             return v
-        for i in range(POW_MAX, -1, -1):
-            if self.parents[i][v] != self.parents[i][u]:
-                v, u = self.parents[i][v], self.parents[i][u]
-        return self.parents[0][v]
+        for i in range(self.depths[v].bit_length() - 1, -1, -1):
+            if self.ancestors[i][v] != self.ancestors[i][u]:
+                v, u = self.ancestors[i][v], self.ancestors[i][u]
+        return self.ancestors[0][v]
