1d3432e37f224ec7bcd04cb1ebe6e2b2e88604b8
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-06-28 02:14:53 +0900

    Add matrix

---
+++ cplib/matrix.py
@@ -0,0 +1,143 @@
+from typing import Iterator, List, Union
+
+
+class Matrix:
+    __slots__ = "inner"
+
+    def __init__(self, n: int, m: int, default: int = 0) -> None:
+        self.inner = [[default] * m for _ in range(n)]
+
+    def __getitem__(self, row_idx: int) -> List[int]:
+        return self.inner[row_idx]
+
+    def __setitem__(self, row_idx: int, row: List[int]) -> None:
+        assert self.cols() == len(row)
+        self.inner[row_idx] = row
+
+    def __iter__(self) -> Iterator[List[int]]:
+        return self.inner.__iter__()
+
+    @classmethod
+    def from_list(cls, lists: List[List[int]]) -> "Matrix":
+        n = len(lists)
+        m = len(lists[0])
+        for i in range(1, n):
+            assert len(lists[i]) == m
+        mat = Matrix(0, 0)
+        mat.inner = lists
+        return mat
+
+    @classmethod
+    def identity(cls, n: int) -> "Matrix":
+        e = Matrix(n, n)
+        for i in range(n):
+            e[i][i] = 1
+        return e
+
+    def __str__(self) -> str:
+        return self.inner.__str__()
+
+    def rows(self) -> int:
+        return len(self.inner)
+
+    def cols(self) -> int:
+        return len(self[0])
+
+    def copy(self) -> "Matrix":
+        ret = Matrix(0, 0)
+        ret.inner = [r.copy() for r in self.inner]
+        return ret
+
+    def _replace(self, other: "Matrix") -> None:
+        for i in range(self.rows()):
+            for j in range(self.cols()):
+                self[i][j] = other[i][j]
+
+    def __eq__(self, other: "object") -> bool:
+        if not isinstance(other, Matrix):
+            return False
+        if not (self.rows() == other.rows() and self.cols() == other.cols()):
+            return False
+        for i in range(self.rows()):
+            for j in range(self.cols()):
+                if self[i][j] != other[i][j]:
+                    return False
+        return True
+
+    def __add__(self, other: "Matrix") -> "Matrix":
+        ret = self.copy()
+        ret += other
+        return ret
+
+    def __iadd__(self, other: "Matrix") -> "Matrix":
+        assert self.rows() == other.rows() and self.cols() == other.cols()
+        for i in range(self.cols()):
+            for j in range(self.rows()):
+                self[i][j] += other[i][j]
+        return self
+
+    def __sub__(self, other: "Matrix") -> "Matrix":
+        ret = self.copy()
+        ret -= other
+        return ret
+
+    def __isub__(self, other: "Matrix") -> "Matrix":
+        assert self.rows() == other.rows() and self.cols() == other.cols()
+        for i in range(self.cols()):
+            for j in range(self.rows()):
+                self[i][j] -= other[i][j]
+        return self
+
+    def __neg__(self) -> "Matrix":
+        return Matrix.from_list(
+            [[-self[i][j] for j in range(self.cols())] for i in range(self.rows())]
+        )
+
+    def __mod__(self, other: int) -> "Matrix":
+        ret = self.copy()
+        ret %= other
+        return ret
+
+    def __imod__(self, mod: int) -> "Matrix":
+        for i in range(self.cols()):
+            for j in range(self.rows()):
+                self[i][j] %= mod
+        return self
+
+    def __mul__(self, other: Union["Matrix", int]) -> "Matrix":
+        """O(n³)"""
+        if isinstance(other, int):
+            return Matrix.from_list(
+                [
+                    [self[i][j] * other for j in range(self.cols())]
+                    for i in range(self.rows())
+                ]
+            )
+
+        h, w = self.cols(), self.rows()
+        u, v = other.cols(), other.rows()
+        assert w == u
+        ret = Matrix(h, v)
+        for i in range(h):
+            for k in range(w):
+                for j in range(v):
+                    ret[i][j] += self[i][k] * other[k][j]
+        return ret
+
+    def __imul__(self, other: Union["Matrix", int]) -> "Matrix":
+        self._replace(self * other)
+        return self
+
+    def pow_mod(self, exp: int, mod: int) -> "Matrix":
+        """self^exp % mod with O(n³log(exp))"""
+        assert self.cols() == self.rows()
+        base = self
+        ret = Matrix.identity(self.cols())
+        while exp:
+            if exp & 1:
+                ret *= base
+                ret %= mod
+            base *= base
+            base %= mod
+            exp >>= 1
+        return ret
---
+++ tests/test_matrix.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+from cplib.matrix import Matrix
+
+
+class Test(TestCase):
+    def test_identity(self) -> None:
+        identity = Matrix.identity(3)
+        assert identity == Matrix.from_list([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    def test_iter(self) -> None:
+        x = Matrix.from_list([[1, 2], [3, 4]])
+        it = iter(x)
+        assert next(it) == [1, 2]
+        assert next(it) == [3, 4]
+        with self.assertRaises(StopIteration):
+            next(it)
+
+    def test_size(self) -> None:
+        x = Matrix.from_list([[1, 2, 3], [3, 4, 5]])
+        assert x.rows() == 2
+        assert x.cols() == 3
+
+    def test_add(self) -> None:
+        x, y = Matrix.from_list([[1, 2], [3, 4]]), Matrix.from_list([[0, 4], [2, 1]])
+        assert x + y == Matrix.from_list([[1, 6], [5, 5]])
+        x += y
+        assert x == Matrix.from_list([[1, 6], [5, 5]])
+
+    def test_sub(self) -> None:
+        x, y = Matrix.from_list([[1, 2], [3, 4]]), Matrix.from_list([[0, 4], [2, 1]])
+        assert x - y == Matrix.from_list([[1, -2], [1, 3]])
+        x -= y
+        assert x == Matrix.from_list([[1, -2], [1, 3]])
+
+    def test_neg(self) -> None:
+        x = Matrix.from_list([[1, 2], [3, 4]])
+        assert -x == Matrix.from_list([[-1, -2], [-3, -4]])
+
+    def test_mod(self) -> None:
+        x = Matrix.from_list([[1, 2], [3, 4]])
+        assert x % 3 == Matrix.from_list([[1, 2], [0, 1]])
+        x %= 3
+        assert x == Matrix.from_list([[1, 2], [0, 1]])
+
+    def test_mul(self) -> None:
+        x, y = Matrix.from_list([[1, 2], [3, 4]]), Matrix.from_list([[0, 4], [2, 1]])
+        assert x * y == Matrix.from_list([[4, 6], [8, 16]])
+        x *= y
+        assert x == Matrix.from_list([[4, 6], [8, 16]])
+
+    def test_pow_mod(self) -> None:
+        x = Matrix.from_list([[1, 2], [3, 4]])
+        x100 = Matrix.identity(2)
+        for _ in range(100):  # naive calculation
+            x100 *= x
+            x100 %= 1223
+        assert x.pow_mod(100, 1223) == x100
