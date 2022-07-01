17611558b2320686f0f3d434cdcb1cc8c5c0919b
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-07-01 11:59:03 +0900

    Update mo

--- cplib/mo.py
+++ cplib/mo.py
@@ -14,30 +14,28 @@ def mo_solve(
     mapping: Callable[[int], T],
 ) -> List[Optional[T]]:
     n = max(map(lambda q: q[1], queries))
-    k = max(n, ceil(n / sqrt(len(queries))))
-
-    queries = list(map(lambda iq: (iq[1][0], iq[1][1], iq[0]), enumerate(queries)))
-
-    def keyfunc(q: Sequence[int]) -> Tuple[int, int]:
-        return (q[0] // k, n - q[1] if (q[0] // k) & 1 else q[1])
-
-    queries.sort(key=keyfunc)
+    sz = max(1, ceil(n / sqrt(len(queries))))
+    buckets: List[List[Tuple[int, int, int]]] = [[] for _ in range(n // sz + 1)]
+    for id in range(len(queries)):
+        buckets[queries[id][0] // sz].append((queries[id][0], queries[id][1], id))
+    for i in range(len(buckets)):
+        buckets[i].sort(key=lambda q: q[1], reverse=bool(i & 1))
 
     ans: List[Optional[T]] = [None] * len(queries)
     cl, cr = 0, 0
-    for l, r, id in queries:
-        while cr < r:
-            extend(cr)
-            cr += 1
-        while l < cl:
-            cl -= 1
-            extend(cl)
-        while r < cr:
-            cr -= 1
-            shrink(cr)
-        while cl < l:
-            shrink(cl)
-            cl += 1
-        v = mapping(id)
-        ans[id] = v
+    for b in buckets:
+        for l, r, id in b:
+            while cr < r:
+                extend(cr)
+                cr += 1
+            while l < cl:
+                cl -= 1
+                extend(cl)
+            while r < cr:
+                cr -= 1
+                shrink(cr)
+            while cl < l:
+                shrink(cl)
+                cl += 1
+            ans[id] = mapping(id)
     return ans
