13835c006ec89f331084e700e401b7eb2f4ceff9
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-07-02 01:24:14 +0900

    Update dbg

--- cplib/dbg.py
+++ cplib/dbg.py
@@ -1,21 +1,38 @@
 import sys
 
 if len(sys.argv) > 0 and sys.argv[0] == "./Main.py":
-
-    def dbg(*objects: object) -> None:
+    # then, it means AtCoder
+    def dbg(*args: object) -> None:
         pass
 
 
 else:
-
     import inspect
     import pathlib
-    import pprint
-    import sys
+    import re
+    from pprint import pformat
+    from typing import List
 
-    def dbg(*objects: object) -> None:
+    def dbg(*args: object) -> None:
         caller = inspect.stack()[1]
+        ctx = caller.code_context
+        s = ctx[0] if ctx else ""
         path = pathlib.Path(caller.filename)
         at = f"{path.parent.name}/{path.name}:{caller.lineno}"
-        repr = pprint.pformat(objects)
-        print(f"{at} => \033[1;41m{repr}\033[0m", file=sys.stderr)
+        matched = re.search(r"dbg\((.*)\)", s)
+        names = []
+        if matched:
+            names = list(map(lambda x: x.strip(), matched[1].split(",")))
+        if len(names) == len(args):
+            vals: List[str] = []
+            for name, arg in zip(names, args):
+                if re.search(r"^'.+'|\".+\"$", name):
+                    vals.append(str(arg))
+                elif str(name) == str(arg):
+                    vals.append(str(arg))
+                else:
+                    vals.append(f"{name}={pformat(arg)}")
+        else:
+            vals = [pformat(arg) for arg in args]
+        cvals = [f"\033[1;41m{v}\033[0m" for v in vals]
+        print(f"{at} => {', '.join(cvals)}", file=sys.stderr)
