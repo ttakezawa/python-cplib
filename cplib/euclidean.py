ffbe21dc569f6c514a62a52f4bf1ecdf99493acf
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-07-05 04:14:41 +0900

    Refactor gcd and lcm

--- cplib/euclidean.py
+++ cplib/euclidean.py
@@ -1,10 +1,16 @@
-def gcd(a: int, b: int) -> int:
-    """O(log min(a,b))"""
-    while b:
-        a, b = b, a % b
-    return a
+from functools import reduce
+import math
 
 
-def lcm(a: int, b: int) -> int:
+def gcd(*integers: int) -> int:
     """O(log min(a,b))"""
-    return a // gcd(a, b) * b
+    return reduce(math.gcd, integers, 0)
+
+
+def lcm(*integers: int) -> int:
+    """O(log min(a,b))"""
+    return reduce(_lcm, integers, 1)
+
+
+def _lcm(a: int, b: int) -> int:
+    return a // math.gcd(a, b) * b
--- tests/test_euclidean.py
+++ tests/test_euclidean.py
@@ -8,8 +8,16 @@ class Test(TestCase):
         assert gcd(12, 8) == 4
         assert gcd(12, 6) == 6
         assert gcd(0, 42) == 42
+        assert gcd(1, 0) == 1
+        assert gcd() == 0
+        assert gcd(3) == 3
+        assert gcd(24, 16, 4) == 4
 
     def test_lcm(self) -> None:
         assert lcm(8, 10) == 40
         assert lcm(3, 5) == 15
         assert lcm(0, 42) == 0
+        assert lcm(1, 0) == 0
+        assert lcm() == 1
+        assert lcm(3) == 3
+        assert lcm(6, 8, 12) == 24
