3672131233f9f98fa84474495334126667a987e1
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-09-10 02:10:10 +0900

    Add bootstrap_with_memo to functools

--- cplib/functools.py
+++ cplib/functools.py
@@ -3,6 +3,7 @@
 # - https://pyrival.readthedocs.io/en/latest/bootstrap.html
 
 from types import GeneratorType
+from typing import Iterator
 
 
 def bootstrap(f, stack=[]):
@@ -21,5 +22,32 @@ def bootstrap(f, stack=[]):
                         break
                     to = stack[-1].send(to)
             return to
+
+    return wrappedfunc
+
+
+def bootstrap_with_memo(f, stack=[], cache={}):
+    def wrappedfunc(*args, **kwargs):
+        key = args + tuple(kwargs.items())
+        if stack:
+            stack.append(key)
+            if key in cache:
+                return iter([cache[key]])
+            return f(*args, **kwargs)
+        if key in cache:
+            return cache[key]
+        to = f(*args, **kwargs)
+        while True:
+            if isinstance(to, Iterator):
+                stack.append(to)
+                to = next(to)
+            else:
+                stack.pop()
+                if not stack:
+                    break
+                cache[stack.pop()] = to
+                to = stack[-1].send(to)
+        cache[key] = to
+        return to
 
     return wrappedfunc
