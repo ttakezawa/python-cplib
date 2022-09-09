cd7eb45a6c4301adea39761637990dc8bbef89bd
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-09-10 02:16:09 +0900

    Add test of bootstrap_with_memo

---
+++ tests/test_functools.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from cplib.functools import bootstrap_with_memo
+
+
+class Test(TestCase):
+    def test_bootstrap_with_memo(self) -> None:
+        @bootstrap_with_memo
+        def fib(n):
+            if n <= 1:
+                yield n
+            a = yield fib(n - 1)
+            b = yield fib(n - 2)
+            yield a + b
+
+        assert fib(50) == 12586269025
