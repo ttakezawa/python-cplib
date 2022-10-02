b87acd5e2b5d6490d4acdd314dbd27c5df6f6229
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-10-02 18:00:51 +0900

    Rename rot_90 -> build_rot90

--- cplib/grid.py
+++ cplib/grid.py
@@ -3,5 +3,5 @@ from typing import List, TypeVar as _TypeVar
 _T = _TypeVar("_T")
 
 
-def rot_90(grid: List[List[_T]]) -> List[List[_T]]:
+def build_rot90(grid: List[List[_T]]) -> List[List[_T]]:
     return [list(reversed(v)) for v in zip(*grid)]
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
-from cplib.grid import rot_90
+from cplib.grid import build_rot90
 
 
 class Test(TestCase):
     def test_rot_90(self) -> None:
         src = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        assert rot_90(src) == [[6, 3, 0], [7, 4, 1], [8, 5, 2]]
+        assert build_rot90(src) == [[6, 3, 0], [7, 4, 1], [8, 5, 2]]
         src = [[0, 1, 2], [3, 4, 5]]
-        assert rot_90(src) == [[3, 0], [4, 1], [5, 2]]
+        assert build_rot90(src) == [[3, 0], [4, 1], [5, 2]]
