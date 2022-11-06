5fd717233dddf327b42f368cbfa47cf4f637f55c
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-11-06 11:19:14 +0900

    Add test of prev_permutation

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from cplib.math import next_permutation
+from cplib.math import next_permutation, prev_permutation
 
 
 class Test(TestCase):
@@ -17,3 +17,18 @@ class Test(TestCase):
         xs = ["a", "bb", "c"]
         assert next_permutation(xs) == True
         assert xs == ["a", "c", "bb"]
+
+    def test_prev_permutation(self) -> None:
+        xs = [3, 2, 1]
+        expects = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+        expects.reverse()
+        for expect in expects[:-1]:
+            assert xs == expect
+            assert prev_permutation(xs) == True
+        assert xs == expects[-1]
+        assert prev_permutation(xs) == False
+        assert xs == expects[-1]
+
+        xs = ["a", "c", "bb"]
+        assert prev_permutation(xs) == True
+        assert xs == ["a", "bb", "c"]
