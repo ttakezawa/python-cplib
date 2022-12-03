0fe6e174187cfaf81787364e857e83ee0483b244
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2022-12-04 03:03:06 +0900

    Add PotentialDSU

---
+++ cplib/potential_dsu.py
@@ -0,0 +1,69 @@
+from typing import List
+
+
+class PotentialDSU:
+    def __init__(self, n: int = 0) -> None:
+        self._n = n
+        self.parent_or_size = [-1] * n
+        self.potential = [0] * n
+
+    def merge(self, a: int, b: int, diff: int) -> int:
+        """b is greater than a by diff: potental[b] = potential[a] + diff"""
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        x = self.leader(a)
+        y = self.leader(b)
+        diff += self.potential[a] - self.potential[b]
+
+        if x == y:
+            return x
+
+        if -self.parent_or_size[x] < -self.parent_or_size[y]:
+            x, y, diff = y, x, -diff
+
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+        self.potential[y] = diff
+
+        return x
+
+    def diff(self, a: int, b: int) -> int:
+        """returns the difference between a and b. (diff(a,b) = potential[b] - potential[a])"""
+        # leader should be called here
+        if not self.same(a, b):
+            raise ValueError(f"a and b ({a} and {b}) is not same group: ")
+        return self.potential[b] - self.potential[a]
+
+    def same(self, a: int, b: int) -> bool:
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        cum = 0
+        leader = a
+        while self.parent_or_size[leader] >= 0:
+            cum += self.potential[leader]
+            leader = self.parent_or_size[leader]
+        while a != leader:
+            self.potential[a], cum = cum, cum - self.potential[a]
+            self.parent_or_size[a], a = leader, self.parent_or_size[a]
+        return leader
+
+    def size(self, a: int) -> int:
+        assert 0 <= a < self._n
+
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self) -> List[List[int]]:
+        leader_buf = [self.leader(i) for i in range(self._n)]
+
+        result: List[List[int]] = [[] for _ in range(self._n)]
+        for i in range(self._n):
+            result[leader_buf[i]].append(i)
+
+        return list(filter(lambda r: r, result))
---
+++ tests/test_potential_dsu.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from cplib.potential_dsu import PotentialDSU
+# Verify
+# - https://onlinejudge.u-aizu.ac.jp/problems/DSL_1_B
+# - https://atcoder.jp/contests/abc280/tasks/abc280_f
+
+
+class Test(TestCase):
+    def test_dsu(self) -> None:
+        dsu = PotentialDSU(6)
+        dsu.merge(0, 4, 1)
+        dsu.merge(1, 2, 1)
+        dsu.merge(2, 3, 1)
+        assert dsu.same(0, 4) == True
+        assert dsu.same(1, 3) == True
+        assert dsu.same(5, 5) == True
+        assert dsu.same(0, 5) == False
+        assert dsu.groups() == [[0, 4], [1, 2, 3], [5]]
+
+        assert dsu.size(0) == 2
+        assert dsu.size(1) == 3
+        assert dsu.size(2) == 3
+        assert dsu.size(3) == 3
+        assert dsu.size(4) == 2
+
+        assert dsu.diff(0, 4) == 1
+        assert dsu.diff(1, 3) == 2
