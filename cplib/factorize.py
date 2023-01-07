8046c3d6819d1cf1adeb8b8afa7661a37f3671d6
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2023-01-08 03:13:43 +0900

    Update factorize

--- cplib/factorize.py
+++ cplib/factorize.py
@@ -5,12 +5,15 @@ from typing import Dict as _Dict
 def factorize(n: int) -> _Dict[int, int]:
     """O(√n)"""
     ret: _Dict[int, int] = __defaultdict(int)
-    i = 2
+    while n & 1 == 0:
+        ret[2] += 1
+        n >>= 1
+    i = 3
     while i * i <= n:
         while n % i == 0:
             ret[i] += 1
             n //= i
-        i += 1
+        i += 2
     if n > 1:
         ret[n] = 1
     return ret
--- tests/test_factorize.py
+++ tests/test_factorize.py
@@ -8,3 +8,4 @@ class Test(TestCase):
         assert factorize(1) == dict()
         assert factorize(2) == {2: 1}
         assert factorize(20) == {2: 2, 5: 1}
+        assert factorize(2023) == {7: 1, 17: 2}
